81860c3cca7ea5925f61ee67e035cfc32fc8413a
RadoslawP <radoslaw.pietraszek.r@gmail.com>
2020-06-24 08:07:20 +0200

    wybieranie najwiekszej z

---
+++ wybieranie_najwiekszej.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int a,b,c,m;
+
+
+int main()
+{
+    cout << "Podaj 3 liczby rozdzielone spacja: ";
+    cin>>a>>b>>c;
+
+    m=a;
+    if(b>m)m=b;
+    if(c>m)m=c;
+
+    cout<<"Najwiêksza liczba to "<<m;
+
+    return 0;
+}
